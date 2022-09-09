c6e6ee87e2d9bbc66bb44abd4b8d363736d1e8e9
Piotr Ostrowski <piotrostr@google.com>
2022-09-09 19:22:37 +0100

    fix and chmod +x the cleanup script

--- hack/clean-up.sh
+++ hack/clean-up.sh
@@ -4,7 +4,8 @@ instance=database
 cluster_name=gke-connectivity-test
 
 gcloud spanner instances delete $instance \
-  --config=regional-us-central1
+  --quiet
 
 gcloud container clusters delete $cluster_name \
-  --regoin=us-east1
+  --region=us-east1 \
+  --quiet
