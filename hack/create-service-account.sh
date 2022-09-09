161506b38dd5833a81cefc508f9c01e9c8f2ad82
Piotr Ostrowski <piotrostr@google.com>
2022-09-09 19:15:58 +0100

    feat: add kubernetes service account and bind it

--- hack/create-service-account.sh
+++ hack/create-service-account.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-gcloud iam service-accounts create spanner-rw \
-  --display-name "Spanner Read Write" \
-  --project piotrostr-resources
+# based on https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity
 
-gcloud projects add-iam-policy-binding piotrostr-resources \
-  --member "serviceAccount:spanner-rw@piotrostr-resources.iam.gserviceaccount.com" \
+gsa_name=gcp-spanner-admin
+gsa=gcp-spanner-admin@piotrostr-resources.iam.gserviceaccount.com
+ksa=kube-spanner-admin
+project_id=piotrostr-resources
+namespace=default
+
+# create kubernetes service account (KSA)
+kubectl create serviceaccount kube-spanner-admin \
+    --namespace default
+
+# create google service account (GSA)
+gcloud iam service-accounts create $gsa_name \
+  --project $project_id
+
+# add GSA permissions
+gcloud projects add-iam-policy-binding $project_id \
+  --member "serviceAccount:$gsa" \
   --role "roles/spanner.databaseAdmin"
+
+# add GSA workloadIdentityUser for KSA
+gcloud iam service-accounts add-iam-policy-binding $gsa \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:piotrostr-resources.svc.id.goog[$namespace/$ksa]"
+
+# annotate the KSA
+kubectl annotate serviceaccount $ksa \
+    --namespace $namespace \
+    iam.gke.io/gcp-service-account=$gsa
