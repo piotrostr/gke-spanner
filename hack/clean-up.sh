0572e14edafcf398cb0ab05ebfab93f49eeaa266
Piotr Ostrowski <piotrostr@google.com>
2022-09-11 21:12:16 +0100

    also remove the service account iam binding as part of the cleanup

--- hack/clean-up.sh
+++ hack/clean-up.sh
@@ -2,10 +2,20 @@
 
 instance=database
 cluster_name=gke-connectivity-test
+gsa=gcp-spanner-admin@piotrostr-resources.iam.gserviceaccount.com
+namespace=default
+ksa=kube-spanner-admin
 
+# remove the service account binding
+gcloud iam service-accounts remove-iam-policy-binding $gsa \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:piotrostr-resources.svc.id.goog[$namespace/$ksa]"
+
+# delete the spanner instance
 gcloud spanner instances delete $instance \
   --quiet
 
+# delete the cluster
 gcloud container clusters delete $cluster_name \
   --region=us-east1 \
   --quiet
